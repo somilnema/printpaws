3f5f03b2b608942affbb7fbc30a1fc113784fc10
somilnema <somilnema29@gmail.com>
2026-04-03 23:29:23 +0530

    feat: improve unboxing experience carousel with smooth animations

--- src/components/UnboxingCarousel.tsx
+++ src/components/UnboxingCarousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { Play, ChevronLeft, ChevronRight } from "lucide-react";
